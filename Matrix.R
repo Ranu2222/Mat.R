c41bcf300e0205b196566c58d4b27cfc9424c239
Ranu2222 <anshumahi2012@gmail.com>
2016-04-17 11:22:37 -0500

    edit

--- Matrix.R
+++ Matrix.R
@@ -1,3 +1,8 @@
+##Catching the Inverse of a Matrix
+##Below are functions to create a special objec that stores a matrix and caches its inverse
+
+## This fnction creates a special "matrix object that cache its inverse
+
 makeCacheMatrix <- function(x = matrix())
 {
 sub <- NULL
@@ -12,6 +17,9 @@ getInverse <- function()sub
 list(set= set, get = get, setInverse = setInverse,getInverse = getInverse)
 }
 
+
+##This function computes the inverse of the special "matrix" created above function. 
+## it sees if the inverse exists , it retrieves the inverse from the cache.
 catcheSolve <- function(x,...)
 {
 sub  <- x$getInverse()
